3f78fcd5129f20c8b9b6b67cfed0073f1d227a62
brayanwhite362-cpu <brayanwhite362@gmail.com>
2026-03-14 21:36:21 +0530

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once "../config/init.php";
-require_once "../config/db.php";
+require_once __DIR__ . "../config/init.php";
+require_once __DIR__ . "../config/db.php";
 
 if (!isset($_SESSION["user_id"]) || ($_SESSION["user_role"] ?? "") !== "admin") {
     header("Location: ../auth/login.php");
