598af4fe7223c81bf6a5760979db26329ca96393
brayanwhite362-cpu <brayanwhite362@gmail.com>
2026-03-14 22:08:55 +0530

    Update mock_payment.php

--- mock_payment.php
+++ mock_payment.php
@@ -1,6 +1,6 @@
 <?php
-require_once "../config/init.php";
-require_once "../config/db.php";
+require_once "config/init.php";
+require_once "config/db.php";
 
 // Check if user is logged in
 if (!isset($_SESSION["user_id"])) {
